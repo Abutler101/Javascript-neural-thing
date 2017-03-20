b04555136fdca978cf46dbe8af6b59fe7b6f218d
Abutler101 <alex@Alex.home>
2017-03-20 20:29:39 +0000

    Trying to chase down the illusive NaN errors that stem from node 5 on layer 2

--- activator.js
+++ activator.js
@@ -1,14 +1,14 @@
 function Sig2(I1,I2){
-  if(I1 == NaN){I1=0.2};
-  if(I2 == NaN){I2=0.2};
+  I1 = +I1.toFixed(7);
+  I2 = +I2.toFixed(7);
   var combo = I1+I2;
   var result = 1/(1+exp(combo));
   return result;
 }
 function Sig3(I1,I2,I3){
-  if(I1 == NaN){I1=0.2};
-  if(I2 == NaN){I2=0.2};
-  if(I3 == NaN){I3=0.2};
+  I1 = +I1.toFixed(7);
+  I2 = +I2.toFixed(7);
+  I3 = +I3.toFixed(7);
   var combo = I1+I2+I3;
   var result = 1/(1+exp(combo));
   return result;
