7b4b836b3d27deb7cab41371ac921bbbe4daa993
Abutler101 <alex@Alex.home>
2017-03-24 16:52:17 +0000

    setup graph

--- Populate.js
+++ Populate.js
@@ -20,12 +20,12 @@ function Populate(){
     Gen ++;
     console.log(Gen);
     //TO DD: Add graph of scores
-    line(45,525,45,775);
-    line(45,775,900,775);
+//    line(45,525,45,775);
+//    line(45,775,900,775);
     var Temp=0;
     for(i=0;i<PGenProtect.length;i++){Temp+=PGenProtect[i][1]}
     Temp =  Temp/PGenProtect.length;
-    Plot.push(Temp);
+//    Graph();
   }
 }
 
